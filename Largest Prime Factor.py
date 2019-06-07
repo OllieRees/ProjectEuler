7eff7c2698c4a01b7c0571a0d901789df4a529a3
Ollie Rees <ollie.rees@me.com>
2019-06-07 23:21:21 +0100

    Completed the challenge where I had to find the largest prime factor of 600851475143.

---
+++ Largest Prime Factor.py
@@ -0,0 +1,25 @@
+#check if n is prime
+def isPrime(n):
+    for fact in range(2, n):
+        if(n % fact == 0):
+            return False
+    return True
+
+
+# Get the largest prime factor of n
+def largestPrimeFactor(n):
+    if (isPrime(n)):
+        return n
+
+    #thinking about n as a multiplication of primes.
+    fact = 2
+    while (n > 1):
+        while(n % fact == 0):
+            n = n/fact
+        fact += 1
+
+    return fact - 1
+
+if __name__ == '__main__':
+    n = 600851475143
+    print(largestPrimeFactor(n))
