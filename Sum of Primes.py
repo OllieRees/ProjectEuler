645c37598ad74d1b4aaf614a6dd275f9b0e774fd
Ollie Rees <ollie.rees@me.com>
2019-06-16 17:09:00 +0100

    Finished problem where I had to find the sum of all the primes which were below 2 million.

--- Sum of Primes.py
+++ Sum of Primes.py
@@ -1,14 +1,28 @@
+import math
+
 #check if n is prime
 def isPrime(n):
     if (n == 1):
         return False
-    if(n == 2):
-        return True
 
-    for fact in range(2, round(n/4)):
+    for fact in range(2, math.floor(math.sqrt(n)) + 1):
         if(n % fact == 0):
             return False
     return True
+
+def primeSieve(list):
+    for e in list:
+        if(e != 2 and e % 2 == 0):
+            list.remove(e)
+        elif(e != 3 and e % 3 == 0):
+            list.remove(e)
+        elif(e != 5 and e % 5 == 0):
+            list.remove(e)
+        elif(e != 7 and e % 7 == 0):
+            list.remove(e)
+        elif(e != 11 and e % 11 == 0):
+            list.remove(e)
+    return list
 
 #sum primes that are lower than prime_lim
 def sumPrimes(prime_lim):
